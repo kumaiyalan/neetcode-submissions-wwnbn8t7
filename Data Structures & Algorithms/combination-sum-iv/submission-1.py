40df39c4747602f724fd88fd961c33001774b3f8
Kevin Umaiyalan <90924949+kumaiyalan@users.noreply.github.com>
2026-08-31 09:56:41 -0400

    Add: combination-sum-iv - submission-1

---
+++ Data Structures & Algorithms/combination-sum-iv/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        cache = {0 : 1}
+
+        def memo(target):
+            if target in cache:
+                return cache[target]
+            #2 choices
+            total = 0
+            for num in nums:
+                if target - num >= 0:
+                    total += memo(target - num)
+            cache[target] = total
+            return total
+        
+        return memo(target)
